eb3273c0c8fa60e2b1292230c521ec9b5609f8ff
jdauliac <jdauliac@sellsy.com>
2024-01-07 17:19:44 +0100

    build(taskfile): remove useless nix run

--- modules/sources/default.nix
+++ modules/sources/default.nix
@@ -1,3 +1,11 @@
-{pkgs}: let
-  application = import ./application/default.nix {inherit pkgs;};
-in {lib = config: {deploy = application.services.deploy config;};}
+{...}: {
+  imports = [./devShell.nix];
+  perSystem = {
+    system,
+    pkgs,
+    self',
+    ...
+  }: let
+    application = import ./application/default.nix {inherit pkgs;};
+  in {lib = config: {deploy = application.services.deploy config;};};
+}

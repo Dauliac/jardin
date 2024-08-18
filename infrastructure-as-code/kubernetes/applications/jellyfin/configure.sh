6f853804baaa813c9eae1526a65387a5c313f553
jdauliac <jdauliac@sellsy.com>
2024-08-19 00:11:22 +0200

    fix(kube): fix jellyfin entrypoint perms

--- infrastructure-as-code/kubernetes/applications/jellyfin/configure.sh
+++ infrastructure-as-code/kubernetes/applications/jellyfin/configure.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -o pipefail
+# set -o pipefail
 set -o nounset
 
 # NOTE: https://github.com/jellyfin/jellyfin/discussions/7460
 
-readonly URL="http://localhost:8096"
+readonly URL="http://jellyfin:8096"
 readonly CONFIG_PATH="/etc/configure-jellyfin"
 readonly MOVIE_JSON_PATH="$CONFIG_PATH/movies.json"
 readonly TV_SHOW_JSON_PATH="$CONFIG_PATH/tv_show.json"
@@ -23,32 +23,60 @@ load_json() {
     printf "%s" "$json_data"
 }
 
-send_curl_request() {
+send_curl_request_post() {
     local endpoint="$1"
     local data="$2"
 
-    if ! curl -sS --fail "$URL/$endpoint" --data-raw "$data"; then
-        printf "Error: Failed to send request to %s\n" "$URL/$endpoint" >&2
+    if ! curl -sS --fail "$URL$endpoint" --data-raw "$data" -H "Content-Type: application/json"; then
+        printf "Error: Failed to send request to %s\n" "$URL$endpoint" >&2
         return 1
     fi
+}
+
+send_curl_request_get() {
+    local endpoint="$1"
+
+    if ! curl -sS --fail "$URL$endpoint" -H "Content-Type: application/json"; then
+        printf "Error: Failed to send request to %s\n" "$URL$endpoint" >&2
+        return 1
+    fi
+}
+
+# Add retry until function
+retry_until_up() {
+    local -r -i max_attempts="100"
+    local -r -i sleep_interval="1"
+    local -i attempt_num=1
+    until send_curl_request_get "/"
+    do
+        if (( attempt_num == max_attempts ))
+        then
+            echo "Max attempts reached"
+            return 1
+        else
+            echo "Retrying, attempt $(( attempt_num++ ))"
+            sleep $sleep_interval
+        fi
+    done
 }
 
 main() {
     local movie_json
     local tv_show_json
-    source
 
     movie_json=$(load_json "$MOVIE_JSON_PATH") || return 1
     tv_show_json=$(load_json "$TV_SHOW_JSON_PATH") || return 1
 
-    send_curl_request "Startup/Configuration" 'UICulture=en-US&MetadataCountryCode=US&PreferredMetadataLanguage=en' || return 1
-    send_curl_request "Startup/User" "" || return 1
-    send_curl_request "Startup/User" "Name=$JELLYFIN_ADMIN_USER&Password=$JELLYFIN_ADMIN_PASSWORD" || return 1
-    send_curl_request "Library/VirtualFolders?collectionType=movies&refreshLibrary=false&name=Movies" "$movie_json" || return 1
-    send_curl_request "Library/VirtualFolders?collectionType=tvshows&refreshLibrary=false&name=TV%20Shows" "$tv_show_json" || return 1
-    send_curl_request "Startup/Configuration" 'UICulture=en-US&MetadataCountryCode=US&PreferredMetadataLanguage=en' || return 1
-    send_curl_request "Startup/RemoteAccess" 'EnableRemoteAccess=true&EnableAutomaticPortMapping=false' || return 1
-    send_curl_request "Startup/Complete" "" || return 1
+    retry_until_up
+
+    send_curl_request_post "/Startup/Configuration" '{"UICulture":"en-US","MetadataCountryCode":"US","PreferredMetadataLanguage":"en"}' || return 1
+    send_curl_request_get "/Startup/User"
+    send_curl_request_post "/Startup/User" "{\"Name\":\"$JELLYFIN_ADMIN_USER\",\"Password\": \"$JELLYFIN_ADMIN_PASSWORD\"}" || return 1
+    send_curl_request_post "/Library/VirtualFolders?collectionType=movies&refreshLibrary=false&name=Movies" "$movie_json" || return 1
+    # TODO: implement TV Show configuration
+    # send_curl_request "/Library/VirtualFolders?collectionType=tvshows&refreshLibrary=false&name=TV%20Shows" "$tv_show_json" || return 1
+    send_curl_request_post "/Startup/RemoteAccess" '{"EnableRemoteAccess":true,"EnableAutomaticPortMapping":false}' || return 1
+    send_curl_request_post "/Startup/Complete" "" || return 1
 }
 
 main "$@"

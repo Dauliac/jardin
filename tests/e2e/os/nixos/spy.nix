4b5b5afff8c1ac43462db04f5631614d4ad1105e
Dauliac <github.rover427@passmail.net>
2025-02-10 17:41:03 +0100

    test: improve firefox size and fix connect command

--- tests/e2e/os/nixos/spy.nix
+++ tests/e2e/os/nixos/spy.nix
@@ -15,17 +15,32 @@
         "org/gnome/desktop/interface".scaling-factor = lib.mkForce (lib.gvariant.mkUint32 1);
       };
     };
+    programs.firefox = {
+      profiles.default = {
+        settings = {
+          "layout.css.devPixelsPerPx" = lib.mkForce "1";
+        };
+      };
+    };
   };
-  users.users.jardin = {
-    password = "jardin";
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "audio"
-      "video"
-    ];
-    openssh.authorizedKeys.keys = [
-      (builtins.readFile ./id_ed25519.pub)
-    ];
+  users.users = {
+    jardin = {
+      password = "jardin";
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "audio"
+        "video"
+      ];
+      openssh.authorizedKeys.keys = [
+        (builtins.readFile ./id_ed25519.pub)
+      ];
+    };
+    admin = {
+      password = "admin";
+      openssh.authorizedKeys.keys = [
+        (builtins.readFile ./id_ed25519.pub)
+      ];
+    };
   };
 }

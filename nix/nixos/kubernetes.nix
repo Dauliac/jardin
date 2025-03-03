6c8bdc647478b995ea0fdb49f7b5eef15b4efda0
Dauliac <github.rover427@passmail.net>
2025-03-03 12:59:15 +0100

    fix(kube): move gateway-api into base layer

--- nix/nixos/kubernetes.nix
+++ nix/nixos/kubernetes.nix
@@ -31,12 +31,50 @@
     in
     {
       description = "Ensure RKE2 manifests are in place before RKE2 starts";
-      before = [ "rke2-server.service" ]; # Exécute avant RKE2
-      wantedBy = [ "multi-user.target" ]; # Démarrage automatique au boot
+      before = [ "rke2-server.service" ];
+      wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
         ExecStart = "${script}";
       };
     };
+    systemd.services.emplace-kubeconfig =
+    let
+      kubeConfigSrc = "/etc/rancher/rke2/rke2.yaml";
+      kubeConfigDirDst = "/home/admin/.kube";
+      kubeConfigDst = "${kubeConfigDirDst}/config";
+      script = pkgs.writers.writeBash "emplace-kubeconfig" ''
+        set -o errexit
+        set -o pipefail
+        set -o nounset
+        set -x
+
+        main() {
+          mkdir -p ${kubeConfigDirDst}
+          cp -f ${kubeConfigSrc} ${kubeConfigDst}
+          chown -R admin:admin ${kubeConfigDirDst}
+          chmod 400 ${kubeConfigDst}
+        }
+        main
+      '';
+
+    in
+    {
+    description = "Copy RKE2 kubeconfig ${kubeConfigSrc} to ${kubeConfigDst}";
+    after = [
+      "network.target"
+    ];
+    wants = [ "rke2-server.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      ExecStart = script;
+      Restart = "on-failure";
+      RestartSec = 5;
+      StartLimitBurst = 50;
+      StartLimitIntervalSec = 600;
+    };
+  };
 }
